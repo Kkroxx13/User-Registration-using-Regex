9b3efdbed0d5aaec20b472bfd3d31d5dfa5e8290
Karan kanchan <kanchan.karan.17ce7002@gmail.com>
2021-08-16 14:18:48 +0530

    Adding the regex Code

---
+++ Regex/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace RegexImplementation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RegexValidation regex = new RegexValidation();
+            bool validResultName = regex.ValidateFirstName();
+            if(validResultName==true)
+                Console.WriteLine("The name is valid");
+            else
+                Console.WriteLine("The name is invalid");
+
+
+            bool validResultNumber = regex.ValidateMobileNumber();
+            if(validResultNumber==true)
+                Console.WriteLine("The Number is valid");
+            else
+                Console.WriteLine("The number is invalid");
+
+
+            bool validResultEmail = regex.validateEmail();
+            if(validResultEmail==true)
+                Console.WriteLine("The Email is valid");
+            else
+                Console.WriteLine("The Email is invalid");
+
+            bool validPassword = regex.ValidatePassword();
+            if(validPassword==true)
+                Console.WriteLine("The Password is valid");
+            else
+                Console.WriteLine("The password is invalid");
+
+        }
+    }
+}
---
+++ Regex/RegexValidation.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Globalization;
+namespace RegexImplementation
+{
+    public class RegexValidation
+    {
+        public static string NAME_REGEX = "^[A-Z]{1}[A-Za-z]{3,}$";
+        public static string MOBILENUMBER_REGEX = "^[0-9]{10}$";
+        public static string EMAIL_REGEX =@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$";
+        public static string PASSWORD_REGEX = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$";
+
+        public bool ValidateFirstName()
+        {
+          return Regex.IsMatch("karan", NAME_REGEX);
+           
+        }
+
+        public bool ValidateMobileNumber()
+        {
+            return Regex.IsMatch("91589833993", MOBILENUMBER_REGEX);
+        }
+        public bool validateEmail()
+        {
+            return Regex.IsMatch("kkkr0099@gmail.com", EMAIL_REGEX);
+        }
+
+        public bool ValidatePassword()
+        {
+            return Regex.IsMatch("morningstar123",PASSWORD_REGEX);
+        }
+    }
+
+    
+}
